3ac3c643778f393a91c1e7dd38e42829f888f546
Hillary Gospodarek <gospodarekh@gmail.com>
2016-07-24 19:21:10 -0400

    welcome, nav bar done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require materialize-sprockets
-//= require materialize/extras/nouislider
 //= require_tree .
 
 // $(function(){ $(document).foundation(); });
