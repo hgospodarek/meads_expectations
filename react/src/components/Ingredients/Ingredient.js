2209cb6bdc9b46c4e9fa841c0317c5c04b3c2e09
Hillary Gospodarek <gospodarekh@gmail.com>
2016-07-28 23:28:20 -0400

    write readme, remove unnecessary files, remove commented-out code

--- react/src/components/Ingredients/Ingredient.js
+++ react/src/components/Ingredients/Ingredient.js
@@ -9,18 +9,18 @@ const Ingredient = props => {
   }
 
   return (
-        <div id={id} className="ingredient callout small">
-          <div className="row">
-            <div className="columns small-11">
-              <li>
-                {props.amount} {props.unit} {props.name}
-              </li>
-            </div>
-            <div className="columns small-1 text-center icon-button">
-              {maybebutton}
-            </div>
-          </div>
+    <div id={id} className="ingredient callout small">
+      <div className="row">
+        <div className="columns small-11">
+          <li>
+            {props.amount} {props.unit} {props.name}
+          </li>
         </div>
+        <div className="columns small-1 text-center icon-button">
+          {maybebutton}
+        </div>
+      </div>
+    </div>
   );
 };
 
